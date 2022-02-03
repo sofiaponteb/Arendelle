6823389c58ee63141bf17ae55cfa647fc14ba38a
sofiaponteb <aaponte@unal.edu.co>
2022-02-03 17:42:50 -0500

    Mapa interactivo

---
+++ botones-mapa.js
@@ -0,0 +1,78 @@
+// Funciones para mostrar los lugares del mapa interactivo
+
+function clickbtn1(){// se activa cuando se da click al boton
+    // asignar las variables de lo que hay debajo del mapa
+    title = document.getElementById("titulo");
+    text = document.getElementById("descripcion");
+    video = document.getElementById("escena");
+    // cambiar el contenido de debajo del mapa
+    title.innerHTML = "Ice Palace";
+    text.innerHTML = "El Castillo de Hielo fue creado por Elsa la Reina de las Nieves en la película animada de Disney Frozen durante la secuencia de Libre soy en la montaña del norte. Este palacio era donde se suponía que debía vivir en el aislamiento con el fin de proteger a la gente de ella. El muñeco de nieve amenazante y la guardia personal de Elsa, Malvavisco vive en el palacio después de Elsa desocupó.";
+    video.src = "https://www.youtube.com/embed/moSFlvxnbgk";
+}
+
+function clickbtn2(){// se activa cuando se da click al boton
+    // asignar las variables de lo que hay debajo del mapa
+    title = document.getElementById("titulo");
+    text = document.getElementById("descripcion");
+    video = document.getElementById("escena");
+    // cambiar el contenido de debajo del mapa
+    title.innerHTML = "Miner's Mountain";
+    text.innerHTML = "En este lugar se desarrolla la escena inicial de la primera película de Frozen";
+    video.src = "https://www.youtube.com/embed/1TXc2JbCjmw";
+}
+
+function clickbtn3(){// se activa cuando se da click al boton
+    // asignar las variables de lo que hay debajo del mapa
+    title = document.getElementById("titulo");
+    text = document.getElementById("descripcion");
+    video = document.getElementById("escena");
+    // cambiar el contenido de debajo del mapa
+    title.innerHTML = "Turf House";
+    text.innerHTML = "El valle es conocido por el Rey y la Reina de Arendelle y que el mapa mostrado en un antiguo libro. Ambos han oído hablar de trolls místicos y sus poderes, por lo que viajan al Valle con el fin de salvar a Anna luego de que Elsa le golpeara por accidente con su poder. La canción Reparaciones tiene lugar en el Valle. Aquí, los trolls intentan casar a Kristoff y Anna. Se puede suponer que Kristoff ha pasado mucho tiempo en el Valle con su familia adoptiva, desde que fue adoptado por Bulda la misma noche que la familia real de Arendelle arribó.";
+    video.src = "https://www.youtube.com/embed/fTzD-bdMpok";
+}
+
+function clickbtn4(){// se activa cuando se da click al boton
+    // asignar las variables de lo que hay debajo del mapa
+    title = document.getElementById("titulo");
+    text = document.getElementById("descripcion");
+    video = document.getElementById("escena");
+    // cambiar el contenido de debajo del mapa
+    title.innerHTML = "Wandering Oaken's Trading post and sauna";
+    text.innerHTML = "Cuando Anna viaja a las montañas en busca de Elsa, se encuentra el puesto de comercio y entra a comprar algo de ropa cómoda de invierno. En el interior, Kristoff entra y los dos se encuentran. A Oaken aparentemente feliz y tranquilo se enojó después de que Kristoff lo llamó ladrón, y patea Kristoff fuera de la tienda. Anna obtiene la idea de contar con Kristoff para guiara a la Montaña del Norte en la búsqueda de Elsa.";
+    video.src = "https://www.youtube.com/embed/lpzvS0ATxB4";
+}
+
+function clickbtn5(){// se activa cuando se da click al boton
+    // asignar las variables de lo que hay debajo del mapa
+    title = document.getElementById("titulo");
+    text = document.getElementById("descripcion");
+    video = document.getElementById("escena");
+    // cambiar el contenido de debajo del mapa
+    title.innerHTML = "Arendelle Castle";
+    text.innerHTML = "Castillo de Arendelle es la casa de Anna y Elsa en la película Frozen. Se encuentra ubicado en una base de tierra noruega. Debido a un control inestable de Elsa de sus poderes, los castillos jardines estaban considerados fuera de límites a la exterior por sus padres, por la preocupación por la protección del reino, junto con Anna y Elsa también.Pero después de 13 años, Elsa finalmente gana el control de sus poderes, declarando que cerrar nunca más las puertas del castillo.";
+    video.src = "https://www.youtube.com/embed/oqFf2bVdyJE";
+}
+
+function clickbtn6(){// se activa cuando se da click al boton
+    // asignar las variables de lo que hay debajo del mapa
+    title = document.getElementById("titulo");
+    text = document.getElementById("descripcion");
+    video = document.getElementById("escena");
+    // cambiar el contenido de debajo del mapa
+    title.innerHTML = "Earth Giant's Passage";
+    text.innerHTML = "Los Gigantes de Tierra son uno de los cuatro espíritus elementales en el bosque, los otros son el espíritu de fuego Bruni, el espíritu del agua El Nokk y el espíritu del viento Gale. Por lo general, solo deambulan por el bosque por la noche. Durante el día, se los puede encontrar durmiendo cerca de un río furioso. Sus estados latentes se parecen a montañas rocosas.<br> Al igual que los otros espíritus, los Gigantes de la Tierra una vez estuvieron en términos pacíficos con la tribu Northuldra. Sin embargo, cuando estalló una guerra entre Northuldra y los vecinos de Arendellianos, los Gigantes de la Tierra se volvieron hostiles y se volvieron contra los humanos que causaron la destrucción de su bosque. Solo destruyendo la fuente del conflicto, una presa encargada por el rey Runeard, se puede domar a los gigantes.";
+    video.src = "https://www.youtube.com/embed/ANqsh3viKl4";
+}
+
+function clickbtn7(){// se activa cuando se da click al boton
+    // asignar las variables de lo que hay debajo del mapa
+    title = document.getElementById("titulo");
+    text = document.getElementById("descripcion");
+    video = document.getElementById("escena");
+    // cambiar el contenido de debajo del mapa
+    title.innerHTML = "Dark Sea";
+    text.innerHTML = "Ahtohallan se encuentra en el Mar Oscuro. Actúa como una especie de archivo visual. Al proyectar escenas de la historia a lo largo de sus paredes heladas, Ahtohallan permite a sus visitantes aprender todo lo que hay que saber sobre su pasado y las personas conectadas con ellos. Sin embargo, atravesar las cavernas de Ahtohallan puede ser extremadamente traicionero. Como advierte Mil Memorias, los que se adentran demasiado en Ahtohallan se ahogarán (en realidad, se congelarán hasta morir).";
+    video.src = "https://www.youtube.com/embed/PftKk8S8jR4";
+}
